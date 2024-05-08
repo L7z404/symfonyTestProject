86034753f9dd5fae084a7ddf848b016b3728db74
L7z404 <leonelcastillo1014@gmail.com>
2024-05-08 17:40:51 -0600

    adding maker and LuckyController changes

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -8,10 +8,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LuckyController extends AbstractController{
     
-    #[Route('/lucky/number')]
-    public function number(): Response {
-        $number = random_int(0,100);
+    // Define route (thanks to AbstractController)
+    //{variable url} and the name of the route (check it with debug:router)
+    #[Route('/lucky/number/{max}', name: 'app_lucky_number')]
+    public function number(int $max): Response {
+        $number = random_int(0,$max);
         
+        //Thanks to the twig, we can use $this->render 
+        //The first parameter is the path of the template
+        //The second parameter is an array of variables to pass to the template
+        //twig recommends snake_case
         return $this->render('lucky/number.html.twig',[
             'number' => $number,
         ]);
