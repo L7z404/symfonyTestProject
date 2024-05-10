8e76f5ffd019d0982f5cac193bda2a8f6d80b4e9
L7z404 <leonelcastillo1014@gmail.com>
2024-05-09 18:38:03 -0600

    very crappy search function

--- src/Controller/ApiInfoController.php
+++ src/Controller/ApiInfoController.php
@@ -10,13 +10,18 @@ use Symfony\Component\Routing\Attribute\Route;
 class ApiInfoController extends AbstractController
 {
     #[Route('/api/info', name: 'app_api_info')]
-    public function fetchData(ApiService $apiService): Response
+    public function fetchData(): Response
     {
+        return $this->render('api_info/index.html.twig', [
+            'controller_name' => 'ApiInfoController',
+            'data' => null,
+        ]);
+    }
 
-        $data = $apiService->fetchDataApi('https://pokeapi.co/api/v2/pokemon/gulpin');
+    #[Route('/api/info/{name}', name: 'app_api_info_search')]
+    public function fetchDataSearch($name, ApiService $apiService): Response{
 
-
-        // dd($data);
+        $data = $apiService->fetchDataApi('https://pokeapi.co/api/v2/pokemon/'.$name);
 
         return $this->render('api_info/index.html.twig', [
             'controller_name' => 'ApiInfoController',
